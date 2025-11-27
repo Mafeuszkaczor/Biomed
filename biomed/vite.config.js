53552bc3c8a7d7e06a650e78eefd2fe2efcd831e
Mateusz Kaczor <mafeuszkaczor@gmail.com>
2025-11-27 22:23:36 +0100

    fix index

--- biomed/vite.config.js
+++ biomed/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/biomed/',
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Biomed/' : '/',
+  plugins: [vue()]
+}))
+
