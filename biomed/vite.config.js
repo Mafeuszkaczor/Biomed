82beabb8af3cceed18150ec2eff34671adc0cb6e
Mateusz Kaczor <mafeuszkaczor@gmail.com>
2025-11-27 22:35:51 +0100

    Setup deploy

--- biomed/vite.config.js
+++ biomed/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Biomed/' : '/',
-  plugins: [vue()]
-}))
-
+export default defineConfig({
+  base: '/Biomed/',
+  plugins: [vue()],
+})
